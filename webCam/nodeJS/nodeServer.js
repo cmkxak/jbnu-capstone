ab7bf1c5b19f184ad62972af8df30446af7fe424
ljr0683 <ljr0683@naver.com>
2022-06-04 16:48:42 +0900

    StreamingAccessWay Changed

--- webCam/nodeJS/nodeServer.js
+++ webCam/nodeJS/nodeServer.js
@@ -12,12 +12,13 @@ app.set('views', __dirname + '/views'); //dirname == 현재 디렉토리
 app.set('view engine', 'ejs'); // 엔진을 ejs로 설정
 app.engine('html', require('ejs').renderFile); // html 사용, ejs를 가져옴
 
-app.get('/', getTest);
+app.get('/', getEqualId);
 
 app.get('/deeplearning',getdeepLearning);
 
-function getTest(req, res){
+function getEqualId(req, res){
     let id = req.query.id;
+    
     const option = {
         url : "http://210.117.128.200:8080/SeniorValidate",
         qs:{
@@ -25,9 +26,10 @@ function getTest(req, res){
         }
     }
 
-    request(option, function(error, response, html) {
+    request(option, function(error, response, body) {
         if(error) {throw error}
-        const checkAccess = html.trim();
+
+        const checkAccess = body.trim()
 
         if(checkAccess === 'true'){
             res.redirect("http://211.117.125.107:34526/");
