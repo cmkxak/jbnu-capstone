066a990bf008ddd1c546f8dd7e7bef392bb8a2f2
ljr0683 <ljr0683@naver.com>
2022-06-05 20:24:52 +0900

    Get automatic webcam IP

--- webCam/nodeJS/nodeServer.js
+++ webCam/nodeJS/nodeServer.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const request = require('request');
 const app = express();
+const spawn = require('child_process').spawn;
+
+const result = spawn('python', ['getIP.py']);
+
+var ip;
+result.stdout.on('data', function(data) {
+    ip = data.toString().trim()
+})
 
 const server = app.listen(9090, () => {
     console.log('Start Server : localhost:9090');
 });
-
-console.log('start');
 
 app.set('views', __dirname + '/views'); //dirname == 현재 디렉토리
 app.set('view engine', 'ejs'); // 엔진을 ejs로 설정
@@ -18,7 +24,7 @@ app.get('/deeplearning',getdeepLearning);
 
 function getEqualId(req, res){
     let id = req.query.id;
-    
+    //console.log(req.connection.remoteAddress) 들어오는 ip 콘솔에 찍음
     const option = {
         url : "http://210.117.128.200:8080/SeniorValidate",
         qs:{
@@ -32,7 +38,7 @@ function getEqualId(req, res){
         const checkAccess = body.trim()
 
         if(checkAccess === 'true'){
-            res.redirect("http://211.117.125.107:34526/");
+            res.redirect("http://"+ ip +":34526/");
         }
         else{
             res.status(404).render('errorPage.html');
@@ -44,7 +50,7 @@ function getdeepLearning(req, res){
     let deeplearning = req.query.deeplearning;
 
     if(deeplearning === 'capstone2022'){
-        res.redirect("http://211.117.125.107:34526/");
+        res.redirect("http://" + ip + ":34526/");
     }
 }
 
