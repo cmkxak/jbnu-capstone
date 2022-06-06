cdcd2bd459ef28e7869f62440b636282f349bd7b
cmkxak <gcm466@naver.com>
2022-06-06 19:06:23 +0900

    delete interface that can modify senior's info.

--- app/src/main/java/com/example/firsttest/SendEventListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.firsttest;
-
-public interface SendEventListener {
-    public void updateName(String s);
-    public void updateAge(String s);
-    public void updatePhoneNumber(String s);
-}
